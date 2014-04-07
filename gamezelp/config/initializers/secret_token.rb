2f3935e6335c82bc6a355f76a527d945be84e2e6
danielwells <danielwells.usnret@gmail.com>
2014-04-07 10:18:40 -0500

    Added application directory with rails file structure

---
+++ gamezelp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamezelp::Application.config.secret_token = '01ee6c956e8f30418febc9505e6961d6e6743378245eebc669adf025bc44451af6633511931cc5887136c916add0a40e15eca797661bad0dd6f7b8ad3a350253'
