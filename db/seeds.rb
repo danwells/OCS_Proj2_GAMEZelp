97059fcf64381d1044ae2a1c9447f039362e6cab
danielwells <danielwells.usnret@gmail.com>
2014-04-08 16:20:28 -0500

    Finished up initial design for main page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Site.create(name: "IGN", base_url: "www.ign.com")
+Site.create(name: "GameSpot", base_url: "www.gamespot.com")
+Site.create(name: "GiantBomb", base_url: "www.giantbomb.com")
+Site.create(name: "GamesRadar", base_url: "www.gamesradar.com")
+Site.create(name: "Game Guides", base_url: "www.gamepressure.com")
+Site.create(name: "ViedoGames Blogger", base_url: "www.videogamesblogger.com")
+Site.create(name: "GameFAQs", base_url: "www.gamefaqs.com")
