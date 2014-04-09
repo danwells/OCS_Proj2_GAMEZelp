745b2c29a9d28a6bb2e0bc51ec0937ab13b32b07
danielwells <danielwells.usnret@gmail.com>
2014-04-08 19:57:11 -0500

    Found links on one site (IGN) and successfully transferred the link strings to the main view. Need to format into actual links and label with shorter name.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,8 +1,24 @@
+require 'open-uri'
+
 class SitesController < ApplicationController
+
   def index
+    @link_titles = []
+    @link_urls = []
     if params[:gameQuery]
-      puts params[:gameQuery]
+      @ign_site = Site.find_by_name('IGN')
+      query = URI.escape(params[:gameQuery])
+      search_str = "http://" + @ign_site.base_url + "/search?q=" + query
+      doc = Nokogiri::HTML(open(search_str))
+      css_selector = "#search-page #search-list .search-item-title a"
+      results = doc.css(css_selector)
+      results.each do |r|
+        @link_titles << r.children.to_s
+        @link_urls << r.attributes["href"].value
+      end      
     end
+    
     @sites = Site.all
   end
+  
 end
