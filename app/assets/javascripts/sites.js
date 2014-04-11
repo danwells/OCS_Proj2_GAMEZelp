4386c51e943f523a84c1117cc29623fcf141bf8d
danielwells <danielwells.usnret@gmail.com>
2014-04-11 00:09:49 -0500

    Added logo sprites. Sprites change to a different color on various events though JQuery code.

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -5,9 +5,28 @@ $('document').ready(function(){
   $(".navBtn").hover(
     function(){
       $(this).find('.drop_down').slideDown();
+      $('.gz_sprite').removeClass().addClass("gz_sprite gz_purple_logo");
     },
     function(){
       $(this).find('.drop_down').slideUp();
+      $('.gz_sprite').removeClass().addClass("gz_sprite gz_blue_logo");
     });
     
+  $("#logo_container").hover(
+    function(){
+      $(this).find('.gz_sprite').removeClass("gz_blue_logo").addClass("gz_green_logo");
+    },
+    function(){
+      $(this).find('.gz_sprite').removeClass("gz_green_logo").addClass("gz_blue_logo");
+    });
+ 
+  $("form").submit(function(event){
+    $('.gz_sprite').removeClass().addClass("gz_sprite gz_red_logo");
+  });   
+
+  $("a").on("click", function(){
+    $('.gz_sprite').removeClass().addClass("gz_sprite gz_red_logo");
+  });   
+  
+  
 });
